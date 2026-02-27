4afad1c781454767792c36ad3ef0ae882dfb25d7
ashira09 <vecnivtil89@gmail.com>
2026-02-27 15:49:59 +0800

    Исправление неправильного импорта трейта HasFactory

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable
 {
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use HasFactory;
     
     use HasApiTokens;
 
