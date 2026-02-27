0e899c26aeb57c61565e55c8e3762726b5a3cbcf
ashira09 <vecnivtil89@gmail.com>
2026-02-27 17:34:23 +0800

    Создание фабрики для модели Publisher

--- backend/app/Models/Publisher.php
+++ backend/app/Models/Publisher.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Publisher extends Model
 {
+    use HasFactory; // для генерации данных
+    
     protected $primaryKey = 'publisher_id';
     public $timestamps = true;
     protected $fillable = ['publisher_name'];
