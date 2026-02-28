2977ed385ff5431b42d8d74e49003b30e0468c38
ashira09 <vecnivtil89@gmail.com>
2026-02-28 15:34:20 +0800

    Update entrypoint.sh Добавил заполнение БД

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -5,5 +5,5 @@ cp .env.example .env
 touch database/database.sqlite
 composer install
 php artisan key:generate
-php artisan migrate:fresh
+php artisan migrate:fresh --seed
 exec docker-php-entrypoint php-fpm
