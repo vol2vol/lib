f6ced3c912b76103fa2a4abad6077681de446ae8
Aleksei <surkovalex@gamil.com>
2026-02-28 18:54:28 +0800

    CRUD форматов

--- backend/routes/admin.php
+++ backend/routes/admin.php
@@ -3,14 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\BookController;
 
-Route::middleware(['auth:sanctum', 'admin'])->prefix('admin')->group(function () {
+Route::prefix('admin')->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::middleware(['admin'])->group(function () {
 
-    Route::apiResource('books', BookController::class);
+            Route::apiResource('books', BookController::class);
+            Route::apiResource('authors', App\Http\Controllers\Admin\AuthorController::class);
+            Route::apiResource('genres', App\Http\Controllers\Admin\GenreController::class);
+            Route::apiResource('publishers', App\Http\Controllers\Admin\PublisherController::class);
+            Route::apiResource('formats', App\Http\Controllers\Admin\FormatController::class);
 
-    // Позже добавим:
-    // Route::apiResource('authors', App\Http\Controllers\Admin\AuthorController::class);
-    // Route::apiResource('genres', App\Http\Controllers\Admin\GenreController::class);
-    // Route::apiResource('publishers', App\Http\Controllers\Admin\PublisherController::class);
-    // Route::apiResource('formats', App\Http\Controllers\Admin\FormatController::class);
-
+        });
+    });
 });
