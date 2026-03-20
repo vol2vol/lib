6e7e646c36cd7f56f20202fb63bd7bd157a600f9
ashira09 <vecnivtil89@gmail.com>
2026-03-20 10:22:58 +0800

    Обновление laravel.yml и entrypoint.sh - стоит избегать регулярное использования composer update, лучше использовать по необходимости

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -5,7 +5,6 @@ cp .env.example .env
 cp -r database/seeders/books storage/app/private/books
 cp -r database/seeders/covers storage/app/private/covers
 touch database/database.sqlite
-composer update
 composer install
 php artisan key:generate
 php artisan migrate:fresh --seed
